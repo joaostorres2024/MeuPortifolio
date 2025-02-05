dd87110a2325592eb643bedbe29cec948ea0a297
joaostorres2024 <joao.pedrost2004@gmail.com>
2025-02-05 11:01:26 -0300

    Alert atualizado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 window.onload = function() {
-  alert("Atenção: Este site está em construção e pode estar incompleto ou falhas!");
+  alert("Atenção: Este site está em construção e pode estar incompleto ou com falhas!");
 }
